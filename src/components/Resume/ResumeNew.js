2135bab967e3842d334747dde244d9f60a680cbc
Muaz Aqdas <muazaqdasssh@gmail.com>
2022-07-14 03:40:14 +0530

    resume link updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com";
+  "https://drive.google.com/file/d/1c6RZ3ePSRNqdYDPvZXPjgvzF17IRDLIN/view";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -35,7 +35,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
+          <Document file={{resumeLink}} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
