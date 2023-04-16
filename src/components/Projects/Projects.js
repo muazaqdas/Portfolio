25b9614bc335e0ec92f22b16dd912a6d82c095b1
Muaz Aqdas <muazaqdasssh@gmail.com>
2023-04-17 04:07:34 +0530

    added careerfly, n0c and updated emara in project section, moved assets to public

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,14 +2,57 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-// import leaf from "../../Assets/Projects/leaf.png";
-// import emotion from "../../Assets/Projects/emotion.png";
-// import editor from "../../Assets/Projects/codeEditor.png";
-// import suicide from "../../Assets/Projects/suicide.png";
-import agrizone from "../../Assets/Projects/AgriZone1.png";
-import emara from "../../Assets/Projects/Emara1.png";
-import note from "../../Assets/Projects/NoteKeeper1.png";
-import fitness from "../../Assets/Projects/FitnessApp.png";
+
+const projects = [
+  {
+    imgPath: "/Assets/Projects/emara.png",
+    title: "EMARA",
+    description:
+      "Recently completed a freelance project utilizing Next.js, React.js, Bootstrap CSS, and Hygraph to build a multipage responsive portfolio website showcasing various projects for the client. Worked closely with the client to understand their requirements and implemented modern web development technologies and best practices to create a visually appealing and user-friendly website. Ensured seamless performance across different devices and screen sizes, and incorporated responsive design principles to optimize the user experience. Grateful for the opportunity to contribute to the client's online presence and help them showcase their projects in a professional and engaging manner.",
+    // ghLink=""
+    demoLink: "https://emara.vercel.app/",
+  },
+  {
+    imgPath: "/Assets/Projects/n0c.png",
+    title: "n0c",
+    description:
+      "Completed a freelance project using React.js, Bootstrap, and custom CSS to create a landing website for a client's portfolio. Collaborated closely to deliver a modern, visually appealing, and responsive website that effectively showcases their work. Utilized industry-standard technologies and best practices to ensure seamless user experience.",
+    // ghLink=""
+    demoLink: "https://n0c.netlify.app/",
+  },
+  {
+    imgPath: "/Assets/Projects/careerfly.png",
+    title: "Careerfly",
+    description:
+      "Led the development of an innovative Ed-Tech website as a Senior Web Developer at HITGOC. Collaborated with a skilled team to create a clean and responsive user interface that enables seamless enrollment in a diverse range of courses. Implemented cutting-edge web technologies to deliver an intuitive and engaging user experience, ensuring optimal performance across multiple devices. Resulted in an enhanced online learning platform that empowers students to access high-quality educational resources and achieve their learning goals.",
+    // ghLink=""
+    demoLink: "https://careerfly.in/",
+  },
+  {
+    imgPath: "/Assets/Projects/NoteKeeper.png",
+    title: "Note Keeper App",
+    description:
+      "Designed and developed an easy to use notes keeping app. Managed state using react hooks to easily write notes. Add & delete notes easily, just by clicking a button.",
+    ghLink: "https://github.com/muazaqdas/note-keeper-app/",
+    demoLink: "http://note-keepers.netlify.app/",
+  },
+  {
+    imgPath: "/Assets/Projects/AgriZone1.png",
+    title: "AgriZone",
+    description:
+      "An agricultural product selling website build with React.js & Bootstrap.",
+    ghLink: "https://github.com/muazaqdas/AgriZone",
+    demoLink: "https://agrizone.netlify.com/",
+  },
+  {
+    imgPath:  "/Assets/Projects/FitnessApp.png" ,
+    title: "Fitness App:Dashboard",
+    description:
+      "Created an interactive dashboard for a fitness app using Bootstrap & React.Js. Fetching user data (example: user id, name, email) from a json object and displaying it on the dashboard. Added circular-progress-bars and routes for workout & nutrition page for each user.",
+    ghLink: "https://github.com/muazaqdas/Fitness-App-Dashboard",
+    demoLink: "https://fitness-app-dashboard.netlify.app/",
+  },
+];
 
 function Projects() {
   return (
@@ -23,43 +66,19 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={agrizone}
-              title="AgriZone"
-              description="An agricultural product selling website build with React.js & Bootstrap."
-              ghLink="https://github.com/muazaqdas/AgriZone"
-              demoLink="https://agrizone.netlify.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emara}
-              title="EMARA"
-              description="A freelance project build with React.js and Bootstrap Css, showcasing client's various projects. A multipage portfolio website with image carousel."
-              // ghLink=""
-              demoLink="https://emara.netlify.app/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={note}
-              title="Note Keeper App"
-              description="Designed and developed an easy to use notes keeping app. Managed state using react hooks to easily write notes. Add & delete notes easily, just by clicking a button."
-              ghLink="https://github.com/muazaqdas/note-keeper-app/"
-              demoLink="http://note-keepers.netlify.app/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={fitness}
-              title="Fitness App:Dashboard"
-              description="Created an interactive dashboard for a fitness app using Bootstrap & React.Js. Fetching user data (example: user id, name, email) from a json object and displaying it on the dashboard. Added circular-progress-bars and routes for workout & nutrition page for each user."
-              ghLink="https://github.com/muazaqdas/Fitness-App-Dashboard"
-              demoLink="https://fitness-app-dashboard.netlify.app/"
-            />
-          </Col>
+          {projects.map((item, index) => {
+            return (
+              <Col md={4} className="project-card" key={index}>
+                <ProjectCard
+                  imgPath={item.imgPath}
+                  title={item.title}
+                  description={item.description}
+                  ghLink={item?.ghLink}
+                  demoLink={item.demoLink}
+                />
+              </Col>
+            );
+          })}
         </Row>
       </Container>
     </Container>
